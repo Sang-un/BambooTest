7acdcc85777b7e1f0f0624c36e6b7ad9e4c9eda4
Sang-un <dltkddns79@gmail.com>
2022-05-18 18:03:18 +0900

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# 전체 백업
+cd /backup
 
-# vm02의 위치
+DIR=`date +%Y%m`
+if [ ! -d ${DIR} ];then
+        mkdir ${DIR}
+fi
+
+mv ${DIR}*.gz ${DIR}
+
+
 DEST=/backup
 
-# 백업을 수행할 디렉터리
-backup_source="/root/origin"
-
-# 백업을 할 압축 파일명
-bfilename=`date +"%Y%m%d_data.tar.gz"`
-bfilename="/backup/${bfilename}"
-
-
 # tar를 이용하여 백업을 수행
-tar zcvpf ${bfilename} ${backup_source}
-
-cd /backup
+tar zcvpf ${DIR}.tar.gz ${DIR}
 
 FTP_HOST=20.249.22.99
 USERNAME=azureuser
@@ -29,29 +26,3 @@ exit
 
 EOF
 
-~           
-
-#!/bin/bash
-
-
-DEST="/backup"
-
-DIR=`date +"%Y%m"`
-
-
-cd "${DEST}"
-
-
-BACKUP_SOURCE="/root/origin"
-
-if [ ! -d ${DIR} ];then
-        mkdir ${DIR}
-fi
-
-cd "${DIR}"
-
-BACKUP_FILE_NAME=`date +"%Y%m%d_data.tar.gz"`
-BACKUP_FILE_NAME="/backup/${DIR}/${BACKUP_FILE_NAME}"
-
-
-tar zcvpf ${BACKUP_FILE_NAME} ${BACKUP_SOURCE}
